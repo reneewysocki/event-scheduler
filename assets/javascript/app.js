f5ed55945f3542208a51b433eee51dd9555dc8cc
Renee Wysocki <reneew311@gmail.com>
2018-10-01 12:06:06 -0500

    html file setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD1ShP9tvQ2PkdQNcTqIGbwsO01W4tb2_A",
+    authDomain: "train-scheduler-1e7b5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1e7b5.firebaseio.com",
+    projectId: "train-scheduler-1e7b5",
+    storageBucket: "train-scheduler-1e7b5.appspot.com",
+    messagingSenderId: "465644469659"
+  };
+  firebase.initializeApp(config);
